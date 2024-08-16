33d6d52a5e3870cd9f7ea1be847e70b38bbbc04e
Matias <marazza.matias@gmail.com>
2024-08-16 12:05:07 -0300

    Require API_KEY

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
 from app.api.endpoints import rankings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = "access-token"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 app = FastAPI()
 
-# Include routers from the endpoints
-app.include_router(rankings.router, prefix="/rankings", tags=["score"])
+# Dependency to check API key
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+
+# Include routers with the API key dependency
+app.include_router(rankings.router, prefix="/rankings", tags=["score"], dependencies=[Depends(get_api_key)])
 
 @app.get("/")
 def read_root():
