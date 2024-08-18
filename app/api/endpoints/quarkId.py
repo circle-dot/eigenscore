fc0794cbd64290c250c94a30cb0751c45b9329ad
MarazzaM <marazza.matias@gmail.com>
2024-08-18 16:15:32 -0300

    Return 422 if the body is empty

--- app/api/endpoints/quarkId.py
+++ app/api/endpoints/quarkId.py
@@ -6,6 +6,9 @@ router = APIRouter()
 async def submit_data(request: Request):
     try:
         data = await request.json()
-        return {"message": "Data received successfully", "data": data}
+        if not data:  # Check if the request body is empty
+            raise HTTPException(status_code=422, detail="Request body is empty")
+        print('data', data)
+        return {"message": "Data received successfully"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error processing request: {str(e)}")
