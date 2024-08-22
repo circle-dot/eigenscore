154730c0ca475a7b155b9ba334508f38f3246e7d
MarazzaM <marazza.matias@gmail.com>
2024-08-22 16:12:31 -0300

    Add ALLOWED_ORIGINS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, Security, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Depends, HTTPException, Security, WebSocket, WebSocketDisconnect, Request
 from app.api.endpoints import rankings, quarkId
 from app.utils.connection_manager import connection_manager
 import os
@@ -11,6 +11,8 @@ API_KEY = os.getenv("API_KEY")
 API_KEY_NAME = "access-token"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
+ALLOWED_ORIGINS = ["https://creci.stamp.network", "https://ferment.openvino.org"]
+
 app = FastAPI()
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
@@ -19,17 +21,27 @@ async def get_api_key(api_key_header: str = Security(api_key_header)):
     else:
         raise HTTPException(status_code=403, detail="Could not validate credentials")
 
-app.include_router(rankings.router, prefix="/rankings", tags=["score"], dependencies=[Depends(get_api_key)])
-app.include_router(quarkId.router, prefix="/quarkid", tags=["quarkId"])
+async def verify_origin(request: Request):
+    origin = request.headers.get('origin')
+    if origin not in ALLOWED_ORIGINS:
+        raise HTTPException(status_code=403, detail="Origin not allowed")
 
-@app.get("/")
+app.include_router(rankings.router, prefix="/rankings", tags=["score"], dependencies=[Depends(get_api_key)])
+app.include_router(quarkId.router, prefix="/quarkid", tags=["quarkId"], dependencies=[Depends(verify_origin)])
+
+@app.get("/", dependencies=[Depends(verify_origin)])
 def read_root():
     return {"message": "Welcome to Agora"}
 
 @app.websocket("/ws/{invitation_id}")
 async def websocket_endpoint(websocket: WebSocket, invitation_id: str):
-    connection_manager.add_connection(invitation_id, websocket)
     await websocket.accept()
+    origin = websocket.headers.get('origin')
+    if origin not in ALLOWED_ORIGINS:
+        await websocket.close(code=1008)  # 1008 - Policy Violation
+        return
+
+    connection_manager.add_connection(invitation_id, websocket)
     print(f"WebSocket {invitation_id} connected")
     try:
         while True:
